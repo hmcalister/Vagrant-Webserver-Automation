b8f418f8fc60dced40459087436f3b0301cc0687
Hayden McAlister <haydenmcalister49@gmail.com>
2021-07-29 23:12:09 +1200

    Add the start of websites adminsite and usersite

---
+++ www/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page metadata -->
+  <meta charset="utf-8">
+  <title>COSC349 Assignment 1</title>
+  <meta name="description" content="COSC349 Assignment 1">
+  <meta name="author" content="Hayden McAlister">
+
+  <!-- CSS references: from SkeletonCSS -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+
+  <!-- Favicon -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+<body>
+  <?php $page_name="Home";
+  $page_desc="A home page for the assignment";
+  include 'header.php';?>
+  <div class="container">
+    <div class="row">
+      <div class="six column">
+        <h4>Basic Page</h4>
+        <p>This index.html page is a placeholder with the CSS, font and favicon. It's just waiting for you to add some content! If you need some help hit up the <a href="http://www.getskeleton.com">Skeleton documentation</a>.</p>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
