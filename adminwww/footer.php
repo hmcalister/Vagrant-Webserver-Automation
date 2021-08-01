36505c028486b95901142a5e446db25562f262af
Hayden McAlister <haydenmcalister49@gmail.com>
2021-08-01 22:14:20 +1200

    Updated style of admin site, including favicon and footer. Added a small amount of content to home page

--- adminwww/footer.php
+++ adminwww/footer.php
@@ -1,14 +1,16 @@
-<div style="position:fixed;
-bottom: 0px; width:100%;">
+<div id="footer">
+    <hr style="margin-top: 0;">
     <div class="container">
         <div class="row">
             <div class="eight columns">
                 <?php echo '<h1>User: ' . $_SESSION['name'] . '</h1>' ?>
             </div>
-            <div class="four columns">
+            <div class="two columns">
+                <a href="home.php"><button type="button">Home</button></a>
+            </div>
+            <div class="two columns">
                 <a href="logout.php"><button type="button">Logout</button></a>
             </div>
         </div>
-        <hr>
     </div>
 </div>
--- adminwww/home.php
+++ adminwww/home.php
@@ -6,33 +6,64 @@
 session_start();
 // If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['loggedin'])) {
-	header('Location: index.php');
-	exit;
+    header('Location: index.php');
+    exit;
 }
 ?>
 
 <head>
 
-	<!-- Basic Page metadata -->
-	<meta charset="utf-8">
-	<title>COSC349 Assignment 1</title>
-	<meta name="description" content="COSC349 Assignment 1">
-	<meta name="author" content="Hayden McAlister">
+    <!-- Basic Page metadata -->
+    <meta charset="utf-8">
+    <title>Admin - Home</title>
+    <meta name="description" content="COSC349 Assignment 1">
+    <meta name="author" content="Hayden McAlister">
 
-	<!-- CSS references: from SkeletonCSS -->
-	<link rel="stylesheet" href="css/normalize.css">
-	<link rel="stylesheet" href="css/skeleton.css">
+    <!-- CSS references: from SkeletonCSS -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/style.css">
 
-	<!-- Favicon -->
-	<link rel="icon" type="image/png" href="images/favicon.png">
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="images/favicon.png">
 
 </head>
 
 
 <body>
-	<?php $page_name = "Home";
-	include 'header.php'; ?>
-    <?php include 'footer.php'?>
+    <?php $page_name = "Home";
+    include 'header.php'; ?>
+    <div class="container" id="main">
+        <div class="row">
+            <a href="sql.php">
+                <div class="four columns">
+                    <div class="admin-section-button">
+                        <h4>SQL Input</h4>
+                        <img src="images/database.png">
+                    </div>
+                </div>
+            </a>
+
+            <a href="tables.php">
+                <div class="four columns">
+                    <div class="admin-section-button">
+                        <h4>View Tables</h4>
+                        <img src="images/table.png">
+                    </div>
+                </div>
+            </a>
+
+            <a href="admin.php">
+                <div class="four columns">
+                    <div class="admin-section-button">
+                        <h4>Add Admin</h4>
+                        <img src="images/user.png">
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <?php include 'footer.php' ?>
 </body>
 
 </html>
