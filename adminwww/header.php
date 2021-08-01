1b5ad6ccc9d4d253b508d2f51e8f280b473577a8
Hayden McAlister <haydenmcalister49@gmail.com>
2021-08-01 22:46:32 +1200

    Update header so horizontal bar has 100% width on screen, to match footer

--- adminwww/header.php
+++ adminwww/header.php
@@ -1,10 +1,12 @@
-<div class="container" style="padding: 2% 0 0 0;">
-    <div class="row">
-        <div class="eight columns">
-            <h1>Admin Controls</h1>
-        </div>
-        <div class="four columns">
-            <?php echo '<h2>'.$page_name.'</h2>'?>
+<div id="header">
+    <div class="container" style="padding: 2% 0 0 0;">
+        <div class="row">
+            <div class="eight columns">
+                <h1>Admin Controls</h1>
+            </div>
+            <div class="four columns">
+                <?php echo '<h2>' . $page_name . '</h2>' ?>
+            </div>
         </div>
     </div>
     <hr>
