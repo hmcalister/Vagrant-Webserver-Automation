ff88e1d1806c3350c7bbc9adf89c642ee101e430
Hayden McAlister <haydenmcalister49@gmail.com>
2021-08-01 22:18:32 +1200

    Add page to admin site to interact directly with SQL server

---
+++ adminwww/sql-query.php
@@ -0,0 +1,56 @@
+<?php
+if ($_POST) {
+    // Change this to your connection info.
+    $DATABASE_HOST = '192.168.2.12:3306';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = 'root';
+    $DATABASE_NAME = 'webdatabase';
+
+    // Try and connect using the info above.
+    $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+    if (mysqli_connect_errno()) {
+        // If there is an error with the connection, stop the script and display the error.
+        echo ('Failed to connect to MySQL: ' . mysqli_connect_error());
+        return;
+    }
+
+    // Now we check if the data from the login form was submitted, isset() will check if the data exists.
+    if ($_POST['query'] === "") {
+        // Could not get the data that should have been sent.
+        echo('Please enter a query!');
+        return;
+    }
+
+    printf("<h3>%s</h3>", $_POST['query']);
+    $result = mysqli_query($con, $_POST['query']);
+    if (gettype($result) === "boolean") {
+        if ($result) {
+            echo ("Query Successful!");
+        } else {
+            echo ("Query Failed!");
+        }
+    } else {
+        $finfo = $result->fetch_fields();
+
+        echo "<table>"; // start a table tag in the HTML
+
+        //create the table headers as column names
+        echo ('<tr>');
+        foreach ($finfo as $header) {
+            printf("<th>%s</th>", $header->name);
+        }
+        echo ('</tr>');
+        //echo each row as next data
+        while ($row = mysqli_fetch_array($result)) {   //Creates a loop to loop through results
+            echo "<tr>";
+            for ($i = 0; $i < count($row); $i++) {
+                printf("<td>%s</td>", $row[$i]);
+            }
+            echo "</tr>";
+        }
+
+        echo "</table>"; //Close the table in HTML
+    }
+    $con->close();
+}
+?>
---
+++ adminwww/sql.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+    header('Location: index.php');
+    exit;
+}
+?>
+
+<head>
+
+    <!-- Basic Page metadata -->
+    <meta charset="utf-8">
+    <title>Admin - SQL</title>
+    <meta name="description" content="COSC349 Assignment 1">
+    <meta name="author" content="Hayden McAlister">
+
+    <!-- CSS references: from SkeletonCSS -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+
+
+<body>
+    <?php $page_name = "SQL";
+    include 'header.php'; ?>
+
+    <div class="container" id="main">
+        <div class="row">
+            <div class="offset-by-two columns eight columns" style="text-align:center;">
+                <h1>SQL Interface: Webdatabase</h1>
+                <h4 style="color:#CC0000;">Warning: All SQL runs trusted - you have been warned</h4>
+            </div>
+        </div>
+        <div class="row">
+            <form action="sql.php" method="post">
+                <div class="eight columns">
+                    <input type="text" name="query">
+                </div>
+                <div class="offset-by-two-columns two columns">
+                    <input type="submit" value="Query">
+                </div>
+            </form>
+        </div>
+        <div class="row">
+            <div class="twelve columns" style="text-align: center;">
+            <?php include "sql-query.php"; ?>
+            </div>
+        </div>
+
+    </div>
+
+    <?php include 'footer.php' ?>
+</body>
+
+</html>
