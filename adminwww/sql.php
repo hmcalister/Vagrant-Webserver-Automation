e0c84c1e43d62505db9954108fc235f3e46f14d2
Hayden McAlister <haydenmcalister49@gmail.com>
2021-08-01 23:25:09 +1200

    Update documentation to remove stray comment

--- adminwww/sql.php
+++ adminwww/sql.php
@@ -56,18 +56,14 @@ if (!isset($_SESSION['loggedin'])) {
             <?php include "sql-query.php"; ?>
             <?php
             if ($_POST) {
-                // Change this to your connection info.
-                
-            
                 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
                 if ($_POST['query'] === "") {
                     // Could not get the data that should have been sent.
                     echo('Please enter a query!');
-                    return;
-                }
-            
-                printf("<h3>%s</h3>", $_POST['query']);
-                sql_query($_POST['query']);
+                } else {
+                    printf("<h3>%s</h3>", $_POST['query']);
+                    sql_query($_POST['query']);
+                }                
             }
             ?>
             </div>
