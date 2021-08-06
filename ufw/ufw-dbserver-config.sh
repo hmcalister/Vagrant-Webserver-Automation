32440ed8a903f6067fb5b2455470c38d55ec75cd
Dan <skafdan@gmail.com>
2021-08-06 23:05:27 +1200

    Preliminary UFW rules added for dbserver.

--- ufw/ufw-dbserver-config.sh
+++ ufw/ufw-dbserver-config.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
 # Sets firewall for vm running database server
+
+# Deny all incoming and outgoing ports
+ufw default deny incoming
+ufw default deny outgoing
+
+# Allow SSH
+ufw allow SSH
+
+# Allow SQL 
+# Allow incoming request on VM subnet
+ufw allow from 192.168.2.0/24 proto tcp to any port 3306
+#Allow outoging requests on VM subnet
+ufw allow to 192.168.2.0/24 proto tcp port 3306
+
+# Enable firewall
+ufw enable
+
+systemctl status ufw 
+
+
